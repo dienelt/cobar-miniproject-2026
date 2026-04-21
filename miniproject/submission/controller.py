2003fecb97349ff97acfa9d99b4ace30c724f53a
dienelt <romainklein9@gmail.com>
2026-04-21 16:13:11 +0200

    Controller test

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -1,6 +1,40 @@
 import numpy as np
 from miniproject.simulation import MiniprojectSimulation
 
+def odor_intensity_to_control_signal(
+    odor_intensities,
+    attractive_gain=-500,
+    aversive_gain=80,
+):
+    attractive_intensities = np.average(
+        odor_intensities[:, 0].reshape(2, 2), axis=0, weights=[9, 1]
+    )
+    aversive_intensities = np.average(
+        odor_intensities[:, 1].reshape(2, 2), axis=0, weights=[9, 1]
+    )
+    attractive_bias = (
+        attractive_gain
+        * (attractive_intensities[0] - attractive_intensities[1])
+        / attractive_intensities.mean()
+        if attractive_intensities.mean() != 0
+        else 0
+    )
+    aversive_bias = (
+        aversive_gain
+        * (aversive_intensities[0] - aversive_intensities[1])
+        / aversive_intensities.mean()
+        if aversive_intensities.mean() != 0
+        else 0
+    )
+    effective_bias = attractive_bias + aversive_bias
+    effective_bias_norm = np.tanh(effective_bias**2) * np.sign(effective_bias)
+
+    control_signal = np.ones(2)
+    side_to_modulate = int(effective_bias_norm > 0)
+    modulation_amount = np.abs(effective_bias_norm) * 0.8
+    control_signal[side_to_modulate] -= modulation_amount
+    return control_signal
+
 
 class Controller:
     def __init__(self, sim: MiniprojectSimulation):
@@ -13,6 +47,19 @@ class Controller:
         # implement your control algorithm here
         olfaction = sim.get_olfaction(sim.fly.name)
         # get other observations as needed
-        drives = np.array([1.0, 1.0])  # replace with your control logic
+        olfaction_smooth = None
+
+
+        if olfaction_smooth is None:
+            olfaction_smooth = olfaction
+        else:
+            alpha = 0.001
+            olfaction_smooth = (1-alpha) * olfaction_smooth + alpha * olfaction
+        
+        drives = odor_intensity_to_control_signal(olfaction_smooth)
+       
+       
+       
+        #drives = np.array([1, 1])  # replace with your control logic
         joint_angles, adhesion = self.turning_controller.step(drives)
         return joint_angles, adhesion
