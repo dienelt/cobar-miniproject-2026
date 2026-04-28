477855b4f3d288122ba029c7e3f08cf52ea8d4ee
amandine-mnr <amandine.meunier@epfl.ch>
2026-04-28 14:24:29 +0200

    first basic mix of odor taxi and optic flow

--- miniproject/submission/controller.py
+++ miniproject/submission/controller.py
@@ -40,6 +40,31 @@ optic_flow_kws = dict(
     flags=0,
 )
 
+def odor_intensity_to_control_signal(
+    odor_intensities,
+    attractive_gain=-500,
+):
+    attractive_intensities = np.average(
+        odor_intensities[:, 0].reshape(2, 2), axis=0, weights=[5, 5]
+    )
+    attractive_bias = (
+        attractive_gain
+        * (attractive_intensities[0] - attractive_intensities[1])
+        / attractive_intensities.mean()
+        if attractive_intensities.mean() != 0
+        else 0
+    )
+    
+    
+    effective_bias = attractive_bias
+    effective_bias_norm = np.tanh(effective_bias**2) * np.sign(effective_bias)
+
+    control_signal = np.ones(2)
+    side_to_modulate = int(effective_bias_norm > 0)
+    modulation_amount = np.abs(effective_bias_norm) * 0.8
+    control_signal[side_to_modulate] -= modulation_amount
+    return control_signal
+
 class Controller:
     def __init__(self, sim: MiniprojectSimulation):
         # you may also implement your own turning controller
@@ -50,8 +75,19 @@ class Controller:
         self.prev_vision = None
 
     def step(self, sim: MiniprojectSimulation):
-        # implement your control algorithm here
+
         olfaction = sim.get_olfaction(sim.fly.name)
+        olfaction_smooth = None
+
+        if olfaction_smooth is None:
+            olfaction_smooth = olfaction
+        else:
+            alpha = 0.001
+            olfaction_smooth = (1-alpha) * olfaction_smooth + alpha * olfaction
+        
+        odor_drives = odor_intensity_to_control_signal(olfaction_smooth)
+
+        # implement your control algorithm here
         ommatidia_readouts = sim.get_ommatidia_readouts(sim.fly.name)
         curr_vision = self.preprocess_fly_vision(ommatidia_readouts)
         # get other observations as needed
@@ -72,14 +108,17 @@ class Controller:
             # turn = 0.8 * turn + 0.2 * self.prev_turn
             # self.prev_turn = turn
 
-            drives = np.array([
+            obstacle_drives = np.array([
                 1.0 - turn,  #left
                 1.0 + turn   #right
             ])
-            drives = np.clip(drives, 0.2, 1.5) #clip for safety ?
+            
 
         else:
-            drives = np.array([1.0, 1.0])
+            obstacle_drives = np.array([1.0, 1.0])
+
+        drives = obstacle_drives*0.5 + odor_drives*0.5
+        drives = np.clip(drives, 0.2, 1.5) #clip for safety ?
 
         self.prev_vision = curr_vision
 
